59bb4f356cc1026b5496347e71d6e00472c35fac
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-10-31 19:26:21 +0100

    e1

--- Exercises/Exercise1.py
+++ Exercises/Exercise1.py
@@ -1,27 +1,19 @@
+kombination = 0
 """
 Given a list of numbers and a number k, return whether any two numbers from the list add up to k.
 """
 def getK(liste, zahl):
-
+    global kombination
     for i in range (0, len(liste)):
-        for j in range(0 , len(liste)):
+        #kein index out of bounce für j, da i niemals den letzten wert der liste annimmt, da dieser vorher schon kombiniert wurde
+        for j in range(1+i , len(liste)):
             if liste[i] + liste[j] == zahl:
-                print(liste[i] + liste[j])
+                kombination = liste[i] + liste[j]
                 return True
 
     return False
-"""
-noch nicht fertig, evtl in einem durchrutsch lösen t.t
-"""
-def onepass(liste, zahl):
-    test = 0
-    for i in range(0, len(liste)):
-        test += liste[i]
-
-    return False
-
 
 if __name__ == "__main__":
     #Beispielarray
-    array = [10, 15, 3, 7]
-    print(getK(array, 25))
+    array = [10, 15, 4, 7, 1]
+    print("{}{}{}{}".format("Irgendeine Kombination für k möglich? ", getK(array, 8)," : " , kombination))
