ed0ce466947402d30d0bc6754a7a5b8add340edf
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-10 15:25:32 +0100

    countingsort

--- SortingAlg/CountingSort.py
+++ SortingAlg/CountingSort.py
@@ -3,31 +3,31 @@
 
 def count_sort(liste, k):
     c = [0] * (k+1)
-    print(c)
 
+    # die erste stelle kommt 0 mal vor, da sonst später error
     for i in range(1, len(liste)):
         c[liste[i]] = c[liste[i]] +1
 
-    print(c)
     for i in range(1, k):
         c[i] = c[i] + c[i-1]
-    print(c)
+
 
     array = [0] * len(liste)
-    print(array)
-    for i in range(len(liste)-1, 1, -1):
+
+    for i in range(len(liste)-1, 0, -1):
         array[c[liste[i]]] = liste[i]
         c[liste[i]] = c[liste[i]] - 1
+
     return array
 
 
 
 if __name__ == "__main__":
-
-    arr = [5,3,9,9,3,2,6,2,7]
+    arr2 = list()
+    #Braucht an erster Stelle die 0, ansonsten fehlt erstes Element im Ergebnis.
+    arr = [0,9,3,9,9,3,2,6,2,7]
     k = 10
     #count_sort(arr, k)
     newArr = count_sort(arr, k)
-    for i in range(len(newArr)):
-        if newArr[i] == 0: newArr.remove(i)
+    newArr.remove(0)
     print(newArr)
