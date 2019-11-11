2950bec84782c4b12009da2cc97841fe77ee6456
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-11 22:16:04 +0100

    radix sort

--- SortingAlg/CountingSort.py
+++ SortingAlg/CountingSort.py
@@ -14,9 +14,11 @@ def count_sort(liste, k):
 
     array = [0] * len(liste)
 
-    for i in range(len(liste)-1, 0, -1):
+    i = len(liste)-1
+    while i >= 0:
         array[c[liste[i]]] = liste[i]
-        c[liste[i]] = c[liste[i]] - 1
+        c[liste[i]] -= 1
+        i -= 1
 
     return array
 
@@ -25,12 +27,12 @@ def count_sort(liste, k):
 if __name__ == "__main__":
     #Braucht an erster Stelle die 0, ansonsten fehlt erstes Element im Ergebnis.
     arr = [9,3,9,9,3,2,6,2,7]
-    arr.insert(0,0)
+    #arr.insert(0,0)
 
     k = 10
-
+    print(arr)
     #Entferne 0 an erster Stelle um Ausgangsarray = Länge Eingabearray zu haben
     newArr = count_sort(arr, k)
     newArr.remove(0)
-    print("Ergebnis:", end = " ")
+    #print("Ergebnis:", end = " ")
     print(newArr)
--- SortingAlg/RadixSort.py
+++ SortingAlg/RadixSort.py
@@ -11,12 +11,8 @@ class Radixsort:
 
     #mittels stabilem counting sort
     def sortExponent(self, a, k, exp):
-        global arraylen
-        bIndex = 0
-        b = [0] * k
+        b = [0] * (k)
         output = [0] * (len(a)+1)
-        #print(b)
-        #print(output)
 
         for i in range(0, len(a)):
             bIndex = (a[i] // exp) % k
@@ -25,13 +21,16 @@ class Radixsort:
         for i in range(1, k):
             b[i] += b[i - 1]
 
-        for i in range(len(a)-1, 0, -1):
+        i = len(a)-1
+        while i >= 0:
             bIndex = (a[i] // exp) % k
-            output[b[bIndex]] = a[i]
-            b[bIndex] = b[bIndex] - 1
+            output[b[bIndex] - 1] = a[i]
+            b[bIndex] -= 1
+            i-=1
 
         for i in range(0, len(a)):
             a[i] = output[i]
+
 
 
     def radix_sort(self, a):
@@ -39,17 +38,15 @@ class Radixsort:
         m = self.findMax(a)
         exp = 1
         while m // exp > 0:
-
             self.sortExponent(a, 10, exp)
             exp *= 10
             #print(a)
-
-        return a
 
 if __name__ == "__main__":
     obj = Radixsort()
     arr = [88, 3, 9, 12, 3, 2, 6, 2, 15]
     #arr.insert(0,0)
-
+    print(arr)
     #obj.radix_sort(arr)
-    print (obj.radix_sort(arr))
+    obj.radix_sort(arr)
+    print(arr)
