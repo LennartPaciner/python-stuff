66c45167b6cce4b03934fa3c7c643b37aa0f437a
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-11-30 18:17:11 +0100

    cs

--- SortingAlg/CountingSort.py
+++ SortingAlg/CountingSort.py
@@ -1,12 +1,10 @@
-
-
+'''
+Counting Sort
+'''
 def count_sort(liste, k):
     c = [0] * (k)
 
-    #wegen liste+1 wird eine extra 0 am ende des ergebnisses übergeben, aber notwendig sonst index fehler.
-    #letzte stelle der ergebnisliste einfach manuell entfernen bevor man mit dem ergebnis weiterarbeitet
     array = [0] * (len(liste))
-
 
     for i in range(0, len(liste)):
         c[liste[i]] += 1
@@ -19,7 +17,6 @@ def count_sort(liste, k):
         array[c[liste[i]] - 1] = liste[i]
         c[liste[i]] -= 1
         i -= 1
-    print(array)
     return array
 
 
@@ -29,8 +26,7 @@ if __name__ == "__main__":
     arr = [9,3,9,9,3,2,6,2,7]
 
     k = 10
-    #print(arr)
-    #Entferne 0 an erster Stelle um Ausgangsarray = Länge Eingabearray zu haben
+
     newArr = count_sort(arr, k)
 
     print("Ergebnis:", end = " ")
---
+++ SortingAlg/HashOffeneAdressierung.py
@@ -0,0 +1,78 @@
+'''
+Fill a Hash Table size m with set of given Keys for Linear Probing, Quadratic Probing
+and Double Hashing for h'(k) = k and h''(k) = 1 + (k mod (m-1))
+'''
+
+"""
+Hashtabelle von 1 bis m+1. i startet im ersten Iterationsschritt bei 1.
+"""
+def linearProbing(schluessel, m):
+    hashTabelle = {}
+    for i in range(1, m+1):
+        hashTabelle[i] = 0
+    for k in range(len(schluessel)):
+        i = 1
+        result = ((schluessel[k] + i) % m) + 1
+        if hashTabelle[result] != 0:
+            frei = False
+            while frei == False:
+                i+=1
+                result = ((schluessel[k] + i) % m) + 1
+                if hashTabelle[result] == 0:
+                    hashTabelle[result] = schluessel[k]
+                    frei = True
+        else:
+            hashTabelle[result] = schluessel[k]
+    return hashTabelle
+
+"""
+Hashtabelle von 1 bis m+1. i startet im ersten Iterationsschritt bei 1. Konstanten 1 und 3
+"""
+def quadraticProbing(schluessel, m):
+    hashTabelle = {}
+    c1 = 1
+    c2 = 3
+    for i in range(1, m + 1):
+        hashTabelle[i] = 0
+    for k in range(len(schluessel)):
+        i = 1
+        result = ((schluessel[k] + i*c1 + (i**2)*c2) % m) + 1
+        if hashTabelle[result] != 0:
+            frei = False
+            while frei == False:
+                i += 1
+                result = ((schluessel[k] + i*c1 + (i**2)*c2) % m) + 1
+                if hashTabelle[result] == 0:
+                    hashTabelle[result] = schluessel[k]
+                    frei = True
+        else:
+            hashTabelle[result] = schluessel[k]
+    return hashTabelle
+
+def doubleHashing(schluessel, m):
+    hashTabelle = {}
+    c1 = 1
+    c2 = 3
+    for i in range(1, m + 1):
+        hashTabelle[i] = 0
+    for k in range(len(schluessel)):
+        i = 1
+        result = ((schluessel[k] + i * c1 + (i ** 2) * c2) % m) + 1
+        if hashTabelle[result] != 0:
+            frei = False
+            while frei == False:
+                i += 1
+                result = ((schluessel[k] + i * c1 + (i ** 2) * c2) % m) + 1
+                if hashTabelle[result] == 0:
+                    hashTabelle[result] = schluessel[k]
+                    frei = True
+        else:
+            hashTabelle[result] = schluessel[k]
+    return hashTabelle
+
+
+
+if __name__ == "__main__":
+    testliste = [10, 22, 31, 4, 15, 28, 17, 88, 59]
+    print(linearProbing(testliste, 11))
+    print(quadraticProbing(testliste, 11))
