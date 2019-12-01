3ff0c80f2e1bfe29f22de43f7b175230a3fae72d
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-12-01 01:14:20 +0100

    test

--- SortingAlg/test.py
+++ SortingAlg/test.py
@@ -1,11 +1,53 @@
+import random
+import string
+
+def extractMin(A, a):
+    arr = [0] * len(a)  #besitzt häufigkeiten der unique chars aus liste a die am anfang erzeugt wird aus eingabestring
+    for i in range(len(a)):
+        for j in range(len(A)):
+            if a[i] == A[j]: arr[i]+=1
+
+    m = min(arr)
+    if len(a) == 1:
+        return arr[0]
+
+    for k in range(0, len(arr)):
+        if arr[k] == m:
+            result = arr[k]
+            arr.remove(m)
+            a.remove(a[k])
+            break
+
+    return result
+
+
+def huffman(A, a):
+
+    u = extractMin(A, a)
+    v = extractMin(A, a)
+    w = random.choice(string.ascii_lowercase)
+    while w in A:
+        w = random.choice(string.ascii_lowercase)
+    neu = (w * (u+v)) + A
+    A = neu
+    a.append(w)
+
+    if len(a)-1 == 1:
+        return a[0]
+
+    return huffman(A, a)
+
 
 
 if __name__ == "__main__":
-    schlüssel = 59
-    print(schlüssel + 3 * (1+(schlüssel % 10)))
-    print("schleife")
-    for i in range(1,15):
+    A = "bananensaft"
+    a = list(set(A))
+    print(huffman(A,a))
+    b = ['a']
+    #print(len(b))
 
-        print(11 * i)
+    #print(random.choice(string.ascii_lowercase))
+
+
 
 
