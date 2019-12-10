9829711fa2ed4bd4fc69523f2eb7b99bc8dc4154
Lennart Paciner <lpaciner@students.uni-mainz.de>
2019-12-10 01:22:42 +0100

    dijkstra 1 versuch

--- SortingAlg/GraphLinkedList.py
+++ SortingAlg/GraphLinkedList.py
@@ -1,12 +1,3 @@
-'''class Knoten:
-    def __init__(self, data = None):
-        self.data = data
-        self.nextval = None
-
-    def __repr__(self):
-        return str(self.data)
-'''
-#für dijksta alg testen ob diese datenstruktur das lösen kann
 class Graph_LL:
 
     def __init__(self, knotenanzahl):
@@ -46,38 +37,65 @@ class Graph_LL:
             if i == newdata:
                 self.linkedliste[stelle].remove(newdata)
 
+    def dijkstra(self, anfang):
+        distanz = [99999999] * len(self.linkedliste)
+        distanz[anfang] = 0
+        nochzusuchen = [False] * len(self.linkedliste)
+
+        for i in range(len(self.linkedliste)):
+            min_knoten = self.minDistanz(distanz, nochzusuchen)
+            print(min_knoten)
+            nochzusuchen[min_knoten] = True
+
+            for j in range(len(self.linkedliste)):
+                if self.linkedliste[min_knoten][j] > 0 and nochzusuchen[j] == False and distanz[j] > distanz[min_knoten] + self.linkedliste[min_knoten][j]:
+                    distanz[j] = distanz[min_knoten] + self.linkedliste[min_knoten][j]
+            print(distanz)
+        self.printDistanz(distanz)
+
+    def printDistanz(self, distanz):
+        for i in range(len(self.linkedliste)):
+            print(distanz[i], end=" ")
+
+
+    def minDistanz(self, distanz, zusuchen):
+        min = 999999999
+        for i in range(len(self.linkedliste)):
+            #print(zusuchen)
+            if distanz[i] < min and zusuchen[i] == False:
+                min = distanz[i]
+                min_index = i
+        return min_index
 
 if __name__ == "__main__":
-    knoten = ['a', 'b', 'c']
+    '''geeks beispiel eingabe
+    knoten = [0, 4, 0 ,0, 0, 0, 0, 8, 0]
     g = Graph_LL(len(knoten))
     g.newList(knoten)
-    g.insertEnd(['e', 'h', 'a'], 1)
+    g.insertEnd([4, 0, 0, 0, 0, 0, 8, 0], 0)
+    g.insertEnd([0, 8, 0, 0, 0, 0, 11, 0], 1)
+    g.insertEnd([8, 0, 7, 0, 4, 0, 0, 2], 2)
+    g.insertEnd([0, 7, 0, 9, 14, 0, 0, 0], 3)
+    g.insertEnd([0, 0, 9, 0, 10, 0, 0, 0], 4)
+    g.insertEnd([0, 4, 14, 10, 0, 2, 0, 0], 5)
+    g.insertEnd([0, 0, 0, 0, 2, 0, 1, 6], 6)
+    g.insertEnd([11, 0, 0, 0, 0, 1, 0, 7], 7)
+    g.insertEnd([0, 2, 0, 0, 0, 6, 7, 0], 8)
     g.printList()
+    g.dijkstra(0)'''
+    knoten = ['A', 'B', 'C', 'D', 'E']
+    g = Graph_LL(len(knoten))
+    #Dijkstra funktioniert. Halbwegs verstanden aber vllt nochmal vertiefen und versuchen selber nachzuprogrammieren!_!
+    g.insertEnd([0, 100, 0, 50, 0], 0)
+    g.insertEnd([0, 0, 100, 0, 250], 1)
+    g.insertEnd([0, 0, 0, 0, 50], 2)
+    g.insertEnd([0, 100, 0, 0, 250], 3)
+    g.insertEnd([0, 0, 0, 0, 0], 4)
+    print("Kürzeste Distanz von Knoten A ausgehend")
+    for i in range(len(knoten)):
+        print(knoten[i], end=" ")
+    print("")
+    g.dijkstra(0)
 
-
-
-
-    '''
-    g.insertEnd('d', 2)
-    g.insertEnd(['e', 'f'], 0)
-    g.insertKnoten('r', 0, 2)
-    g.insertKnoten('u', 1, 1)
-    g.insertKnoten('v', 1, 1)
-    g.printList()
-    print("-------")
-    g.deleteKnoten('r', 0)
-    g.deleteKnoten('b', 1)
-    g.insertKnoten('x', 1, 0)
-    g.insertKnoten('y', 1, 1)
-    g.insertEnd(['h', 'g'], 1)
-    g.printList()
-
-    linkedliste = [[] for x in range(4)]
-
-       linkedliste[0] = [0,1,2]
-       linkedliste[1] = [1,2]
-       linkedliste[2] = [2,3,4,5,6,7]
-       linkedliste[3] = [1]
-       #print(linkedliste)'''
 
 
